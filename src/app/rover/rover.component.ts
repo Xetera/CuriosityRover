bf9f23ff3b1bff10abb5fc28db2870f8809a22cc
Ali <mythologyali@gmail.com>
2018-03-21 12:35:38 -0700

    rover component

--- src/app/rover/rover.component.ts
+++ src/app/rover/rover.component.ts
@@ -1,26 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {ApiService} from '../services/api.service';
-
+import {Picture} from '../interfaces/nasa';
+import {InfiniteScrollDirective} from 'ngx-infinite-scroll';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import { throttleTime } from 'rxjs/operators';
 @Component({
-    selector: 'app-search-bar',
+    selector: 'rover',
     templateUrl: './rover.component.html',
-    styleUrls: ['./rover.component.css']
+    styles: [
+        `.pictures {
+            height: 20rem;
+            overflow: scroll;
+        }`
+    ],
+    styleUrls: ['./rover.component.css'],
 })
 export class RoverComponent implements OnInit {
     title: string = 'Rover Images';
     message: string;
-
-    constructor(private api: ApiService) {
-    }
+    public pictures: Picture[] = [];
+    public APIReady: boolean = true;
+    constructor(private api: ApiService) {}
 
     ngOnInit() {
-        return this.sendRequest();
+        this.sendRequest();
+        // anonymous function since we don't want to create a new 'this' variable
+        window.addEventListener('scroll',() =>  this.checkScrollPosition());
     }
-
-    sendRequest(): void{
-        this.api.getPictures().then((message: string )=> {
-            this.message = message;
+    public checkScrollPosition(){
+        if ( window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+            console.log('scrolled to the bottom');
+            if (this.APIReady) this.sendRequest();
+        }
+    }
+    public sendRequest(): void {
+        this.APIReady = false;
+        this.api.getPictures().then((message: Picture[])=> {
+            this.pictures = this.pictures.concat(message);
+            console.log(this.pictures);
+            this.api.incrementPage();
         });
+        // I know I could be using throttleTime for this but
+        // I really couldn't figure out how observables work
+        setTimeout(() => {
+            this.APIReady = true;
+            console.log('now ready')
+        }, 2000);
     }
-
 }
